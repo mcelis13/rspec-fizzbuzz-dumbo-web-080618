b135b667aebbd7a648c8a1922d846caee686019f
Madeline Celis <madelinebcelis@gmail.com>
2018-07-20 16:49:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,11 @@
 def fizzbuzz(num)
-  if num % 3 === 0
+  if num % 3 === 0 && num % 5 === 0
+    puts 'FizzBuzz'
+  elsif num % 3 === 0
     puts 'Fizz'
   elsif num % 5 === 0
     puts 'Buzz'
+  else
+    return nil
+  end
 end
